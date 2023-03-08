1467ab8a12b5301b5562dafab67cf6851d385d85
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-03-08 15:23:02 +0100

    404

---
+++ navigation-log/resources/views/errors/404.blade.php
@@ -0,0 +1,8 @@
+@extends('app')
+@section('title', "404 - Lost your way?")
+@section('content')
+<div class="lost__card"><span>404</span>
+<h6>Are You Lost ?</h6>
+<a href="/" class="link--general">You're only one click away from home.</a>
+</div>
+@endsection
